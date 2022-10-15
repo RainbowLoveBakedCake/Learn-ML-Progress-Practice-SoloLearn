1b42f415874e890223fbc82257133c9455406d5b
RainbowBakedCake <44669371+RainbowLoveBakedCake@users.noreply.github.com>
2022-10-15 14:42:44 +0700

    Update ANN-Scikit-Learn.py
    adding correct comment

--- Neural-Network/ANN-Scikit-Learn.py
+++ Neural-Network/ANN-Scikit-Learn.py
@@ -3,10 +3,10 @@ from sklearn.neural_network import MLPClassifier
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=3)
 
-// MLP using max_iter
-// mlp = MLPClassifier(max_iter=1000)
+# MLP using max_iter
+# mlp = MLPClassifier(max_iter=1000)
 
-// MLP using random state and other parameters
+# MLP using random state and other parameters
 mlp = MLPClassifier(max_iter=1000, hidden_layer_sizes=(100,50), alpha=0.0001, solver='adam', random_state=3)
 
 mlp.fit(X_train, y_train)
